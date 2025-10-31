f18ea74b4f4e71ebd7a908d167b7cc55928680a3
Daniel <danihailhabt321@gmail.com>
2025-10-31 00:33:41 -0900

    roles front

--- server/server.js
+++ server/server.js
@@ -11,11 +11,7 @@ app.use(express.json());
 app.use(cors());
 const server = http.createServer(app);
 
-// --- Routes ---
-// Health check route
-//........
 
-// Start server
 const port = process.env.PORT || 5000;
 const host = process.env.HOST || '127.0.0.1';
 server.listen(port, host, () => {
